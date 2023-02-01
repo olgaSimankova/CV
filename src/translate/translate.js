76e59d590028455aa32c626b2b7b965db56c3d2d
Olga Simankova <o.kotlyarova@gmail.com>
2023-02-01 17:50:49 +0300

    feat: add contact and skills section

--- src/translate/translate.js
+++ src/translate/translate.js
@@ -2,9 +2,13 @@ const i18Obj = {
     en: {
         myName: 'Simankova Olga',
         occupation: 'FRONTEND DEVELOPER',
+        contact: 'CONTACT',
+        skills: 'SKILLS',
     },
     ru: {
         myName: 'Симанкова Ольга',
         occupation: 'FRONTEND РАЗРАБОТЧИК',
+        contact: 'КОНТАКТЫ',
+        skills: 'НАВЫКИ',
     },
 };
